86f8c62ee8899da20b68e139fec774552afdc730
ACE <acedapogi@yahoo.com>
2024-09-15 21:25:15 +0800

    Initial Commit

---
+++ src/main/java/com/ace/student/app/student_record_app/StudentRecordAppApplication.java
@@ -0,0 +1,13 @@
+package com.ace.student.app.student_record_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRecordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRecordAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace/student/app/student_record_app/StudentRecordAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.student.app.student_record_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*@SpringBootTest
+class StudentRecordAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
